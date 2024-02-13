e66f2ec28d95e448e281f5bf75a305ec04f52abc
aneroda <aneroda@cleo.com>
2024-02-13 15:38:14 +0200

    first commit

---
+++ src/test/java/com/example/VolunteerWebApp/VolunteerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VolunteerWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
