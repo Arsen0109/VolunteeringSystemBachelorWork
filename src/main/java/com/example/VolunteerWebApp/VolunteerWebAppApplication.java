804eb39fee1b23361b70887a1e12d6213d96ce77
aneroda <aneroda@cleo.com>
2024-02-16 14:23:16 +0200

    add asynchonic to sending emails

--- src/main/java/com/example/VolunteerWebApp/VolunteerWebAppApplication.java
+++ src/main/java/com/example/VolunteerWebApp/VolunteerWebAppApplication.java
@@ -3,8 +3,10 @@ package com.example.VolunteerWebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class VolunteerWebAppApplication {
 
 	public static void main(String[] args) {
